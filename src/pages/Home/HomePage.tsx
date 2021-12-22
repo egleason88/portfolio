1e28fcc78d6345406f708afd90d09728f84c1dc2
egleason88 <94930585+egleason88@users.noreply.github.com>
2021-12-21 20:50:59 -0500

    Update HomePage.tsx

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -19,15 +19,10 @@ const HomePage = () => (
             User Experience Designer
           </Typography>
           <Typography>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            Experienced UX Designer with a demonstrated history of working in the research industry 
+            and higher education. Skilled in User Research and Testing, Wire frames, Mockup, Prototyping and Agile Methodology. 
+            Graduating with a degree in Industrial Design and a Master of Science M.Sc.) in Human Factors from Loughborough University
+            has set me up with careers long desire to imporove a users experience in all areas of design.
           </Typography>
         </Stack>
       </Grid>
